f2d9a34cb6b2a4a2247d033572024bb205181f2b
Michael Luo <michael.luo@berkeley.edu>
2017-02-06 21:34:18 -0800

    Added Git Ignore

---
+++ Template.java
@@ -0,0 +1,18 @@
+/*
+ID: michael274
+LANG: JAVA
+TASK: Template
+*/
+
+import java.io.*;
+import java.util.*;
+
+class Template {
+	public static void main (String [] args) throws IOException {
+		Scanner s = new Scanner(new FileReader("C:/Users/Michael Luo/Documents/Competitive Programming/input.txt"));
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("C:/Users/Michael Luo/Documents/Competitive Programming/output.txt")));
+		//Scanner s = new Scanner(new FileReader("Template.in"));
+		//PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("Template.out")));
+		out.close();                                
+	}
+}
